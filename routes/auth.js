80a30cbc185d58fc84f812ab2b28b907ac930730
Shelly Leung <pontificable@yahoo.com>
2018-08-21 10:04:41 -0700

    jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 const options = { session: false, failWithError: true };
 
 const localAuth = passport.authenticate('local', options);
 
-router.post('/login', localAuth, function(req, res) {
-	return res.json(req.user);
+function createAuthToken(user) {
+	return jwt.sigh({ user }, JWT_SECRET, {
+		subject: user.username,
+		expiresIn: JWT_EXPIRY
+	});
+}
+
+router.post('/login', localAuth, function (req, res) {
+	const authToken = createAuthToken(req.user);
+	return res.json({authToken});
 });
 
 module.exports = router;
